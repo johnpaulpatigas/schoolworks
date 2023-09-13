2e1f1c82ffd4139451d8833ca46d569145618338
John Paul Patigas <johnpaulpatigas@gmail.com>
2023-09-14 02:50:01 +0800

    simpleCalculator: Create main program loop
    Create a while loop to display options and take user input for calculator operations.

--- simpleCalculator/simpleCalculator.py
+++ simpleCalculator/simpleCalculator.py
@@ -13,3 +13,13 @@ def divide(x, y):
     if y == 0:
         return "Division by zero is not allowed"
     return x / y
+
+while True:
+    print("Options:")
+    print("Enter 'add' for addition")
+    print("Enter 'subtract' for subtraction")
+    print("Enter 'multiply' for multiplication")
+    print("Enter 'divide' for division")
+    print("Enter 'quit' to end the program")
+
+    user_input = input(": ")
