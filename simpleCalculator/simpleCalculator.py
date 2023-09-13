3432bd2ebb068c1685519a56788f66d3d63f7ea0
John Paul Patigas <johnpaulpatigas@gmail.com>
2023-09-14 02:45:01 +0800

    simpleCalculator: Implement basic math operations
    Define the add, subtract, multiply, and divide functions to perform corresponding operations. Check for division by zero.

--- simpleCalculator/simpleCalculator.py
+++ simpleCalculator/simpleCalculator.py
@@ -1,1 +1,15 @@
 # simpleCalculator
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Division by zero is not allowed"
+    return x / y
