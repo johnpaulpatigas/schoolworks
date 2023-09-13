d0586ce0c6080b2e82aa3825b4ce65be0b1831b4
John Paul Patigas <johnpaulpatigas@gmail.com>
2023-09-14 03:00:01 +0800

    simpleCalculator: Add user input handling for arithmetic operations
    Handle user input for arithmetic operations, taking two numbers and displaying the result using the corresponding function.

--- simpleCalculator/simpleCalculator.py
+++ simpleCalculator/simpleCalculator.py
@@ -26,3 +26,15 @@ while True:
 
     if user_input == "quit":
         break
+    elif user_input in ("add", "subtract", "multiply", "divide"):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        if user_input == "add":
+            result = add(num1, num2)
+        elif user_input == "subtract":
+            result = subtract(num1, num2)
+        elif user_input == "multiply":
+            result = multiply(num1, num2)
+        elif user_input == "divide":
+            result = divide(num1, num2)
+        print("Result: ", result)
