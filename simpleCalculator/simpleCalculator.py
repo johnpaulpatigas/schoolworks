ea658c1ed593e746813bdb876d13174690d2de7f
John Paul Patigas <johnpaulpatigas@gmail.com>
2023-09-15 21:30:00 +0800

    simpleCalculator: Implement operation dictionary
    Implement an operation dictionary to streamline operation selection.

--- simpleCalculator/simpleCalculator.py
+++ simpleCalculator/simpleCalculator.py
@@ -25,23 +25,24 @@ def display_menu():
 def get_user_input():
     return input(": ")
 
+operations = {
+    'add': add,
+    'subtract': subtract,
+    'multiply': multiply,
+    'divide': divide
+}
+
 while True:
     display_menu()
     user_input = get_user_input()
 
     if user_input == "quit":
         break
-    elif user_input in ("add", "subtract", "multiply", "divide"):
+
+    if user_input in operations:
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
-        if user_input == "add":
-            result = add(num1, num2)
-        elif user_input == "subtract":
-            result = subtract(num1, num2)
-        elif user_input == "multiply":
-            result = multiply(num1, num2)
-        elif user_input == "divide":
-            result = divide(num1, num2)
-        print("Result: ", result)
+        result = operations[user_input](num1, num2)
+        print("Result:", result)
     else:
         print("Invalid input. Please try again.")
