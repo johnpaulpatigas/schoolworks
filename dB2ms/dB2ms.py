1235941523c38b3f98877f9902f41bb15afd7412
John Paul Patigas <johnpaulpatigas@gmail.com>
2023-10-17 21:50:00 +0800

    dB2ms: Enable user interaction
    Integrate user input functionality.

--- dB2ms/dB2ms.py
+++ dB2ms/dB2ms.py
@@ -5,3 +5,8 @@ import math
 def dB2ms(dBps):
     ms = 20 / (dBps * math.log(10)) * 10**3
     return ms
+
+while True:
+    dBps_value = float(input("Enter dB/s value: "))
+    ms_value = dB2ms(dBps_value)
+    print(f"{dBps_value} dB/s is equal to {ms_value:.2f} ms.")
